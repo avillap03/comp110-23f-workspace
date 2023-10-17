b07176e85fee5bdde1cecb495d1e1f9001049bf4
Alejandro Villa <avillap@unc.edu>
2023-10-17 11:50:27 -0400

    Submission

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,20 +3,20 @@
 __author__ = "730621663"
 
 
-def all(input: list[int], given_int: int) -> bool:
-    """Return a bool indicating wether or not all the ints in the list are the same as the given int"""
+def all(input: list[int], given_num: int) -> bool:
+    """Return a bool indicating wether or not all the ints in the list are the same as the given int."""
     if len(input) == 0:
         return False
     i: int = 0
-    while int < len(input):
-        if input[int] != given_int:
+    while i < len(input):
+        if input[i] != given_num:
             return False
         i += 1
     return True 
 
 
 def max(input: list[int]) -> int:
-    """Return the largest int in the list"""
+    """Return the largest int in the list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     i: int = 0
@@ -29,9 +29,9 @@ def max(input: list[int]) -> int:
 
 
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
-    """Return bool indicating if every element at every idx is equal in boths lists"""
+    """Return bool indicating if every element at every idx is equal in boths lists."""
     if list_1 == list_2:
         return True
-    else: # list_1 != list_2
+    else: 
         return False
     
