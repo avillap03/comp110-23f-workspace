1104f47ab995cff706488587c6a808972314364c
Alejandro Villa <avillap@unc.edu>
2023-11-27 13:01:29 -0500

    Progress

--- lessons/recursion/node.py
+++ lessons/recursion/node.py
@@ -22,11 +22,20 @@ class Node:
         else:
             return f"{self.data} -> {self.next}"
         
-    def head(self):
-        return None
+    def head(self) -> int:
+        """Return data of the head node."""
+        return self.data
     
-    def tail(self):
-        return None
+    def tail(self) -> Node | None:
+        """Return linked list excluding the head node."""
+        if self.next is None:
+            return None
+        else:
+            return None
     
-    def last(self):
-        return None
+    def last(self) -> int:
+        """Return data of the last elem of the linked list."""
+        if self.next is None:
+            return self.data
+        else:
+            return self.next.last()
